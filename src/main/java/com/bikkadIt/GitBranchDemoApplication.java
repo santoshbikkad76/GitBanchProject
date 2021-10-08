9bc85003d2c59b169e9aabc0fe0a1867ab04152e
GanRaaJ <you@example.com>
2021-10-08 21:09:01 +0530

    IES-04:String variable added in main class

--- src/main/java/com/bikkadIt/GitBranchDemoApplication.java
+++ src/main/java/com/bikkadIt/GitBranchDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitBranchDemoApplication {
-
+        String s1="Bikkad";
 	public static void main(String[] args) {
 		SpringApplication.run(GitBranchDemoApplication.class, args);
 	}
